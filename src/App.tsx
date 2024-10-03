44c547909e75b9e42bde67d84b5ba1e528eaca32
Viacheslav-ua <atph@ukr.net>
2024-10-03 19:03:02 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { TodoPage } from "./pages/TodoPage"
+
+const App = () => {
+
+  return (
+    <TodoPage />
+  )
+}
+
+export default App
---
+++ src/pages/TodoPage.tsx
@@ -0,0 +1,9 @@
+
+export const TodoPage = () => {
+  
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-red-500">Todo Page</h1>
+    </div>
+  )
+}
