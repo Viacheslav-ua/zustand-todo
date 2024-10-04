c16b4e1cf13575144df49cdec86dca3a95dc20bf
Viacheslav-ua <atph@ukr.net>
2024-10-04 12:40:50 +0300

    added InputPlus component

--- src/modules/todo/Todo.tsx
+++ src/modules/todo/Todo.tsx
@@ -1,26 +1,30 @@
-import { useEffect } from "react"
 import { useTodoStore } from "./store/useTodoStore"
+import { InputPlus } from "./components/InputPlus"
 
 export const Todo = () => {
 
   const tasks = useTodoStore(state => state.tasks)
   const addTask = useTodoStore(state => state.addTask)
-    // ({
-    // tasks: store.tasks,
+
+  // tasks: store.tasks,
     // store.addTask,
     // store.updateTask,
     // store.removeTask,
-// }))
-  
-  useEffect(() => {
-    addTask("test")
-  }, [])
   
   console.log(tasks);
+
+  const handleAddTask = (title: string) => {
+    if (title) {
+      addTask(title)
+    }
+  }
+
   return (
-    <article className="bg-white min-w-[600px] rounded-[20px] px-[55px] pt-[20px] flex items-center justify-center">
+    <article className="bg-white min-w-[600px] rounded-[20px] px-[55px] py-[20px] flex flex-col items-center justify-center">
       <h1 className="text-[40px] mb-[0.3em]">To Do App</h1>
-      <section></section>
+      <section className="w-full">
+        <InputPlus addTask={ handleAddTask } />  
+      </section>
       <section></section>
     </article>
   )
--- src/modules/todo/components/InputPlus.tsx
+++ src/modules/todo/components/InputPlus.tsx
@@ -1,1 +1,32 @@
 import { Plus } from 'lucide-react';
+import { useState } from 'react';
+
+interface InputPlusProps {
+  addTask: (title: string) => void
+}
+
+export const InputPlus: React.FC<InputPlusProps> = ({ addTask }) => {
+
+  const [valueTitle, setValueTitle] = useState('');
+
+  return (
+    <div className='w-full flex'>
+      <input 
+        type="text" 
+        placeholder="here you can add a new task ..." 
+        onChange={(event) => setValueTitle(event.target.value)}
+        onKeyDown={(event) => event.key === 'Enter' && addTask(valueTitle)}
+        value={valueTitle}
+        className='border-[1px] border-[#b1b1b1] bg-[#dbe2ef] rounded-l-[5px] px-3 py-2 w-full
+        placeholder:text-[7b7b7b] outline-none focus:border-blue-500 grow'
+        />
+      <button
+        onClick={() => addTask(valueTitle)}
+        aria-label="Add task"
+        title="Add task"
+        className='border-none w-[60px] flex justify-center items-center rounded-r-[5px] 
+        bg-[#3f72af] hover:bg-blue-600 transition-all text-white'
+        ><Plus size={28} /></button>
+    </div>
+  )
+}
